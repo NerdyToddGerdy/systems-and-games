7a3e41635a28ee4f79e64cdde42375798fb68b03
Todd Gerdy <toddgerdy@Todds-iMac.local>
2017-04-18 23:00:23 -0400

    I have finally made my show page for the games to work.

--- controllers/games.js
+++ controllers/games.js
@@ -28,33 +28,36 @@ router.get('/:id', function(req, res){
    var newSysArr = [];
    System.find({}, function(err, foundSystem){
       // console.log(foundSystem);
-      Game.findById(req.params.id, function(err, foundGames){
-         for (var i = 0; i < foundGames.systems.length; i++) {
-            // console.log(foundGames._id);
-            if (System.findById(foundGames.systems[i])){
-               newArr.push(foundGames.systems[i]);
-               // console.log(newArr);
-            }
-            if (foundGames.systems.length < 2) {
-               for (var j = 0; j < foundSystem.length; j++) {
-                  // console.log('%%%%%%%%%%%%%%%%%%%%');
-                  // console.log(newArr[i]);
-                  // console.log(foundSystem[j]._id);
-                  // console.log('does they equal');
-                  if (newArr[i] == foundSystem[j]._id){
-                     // console.log('yes');
-                     // console.log(foundSystem[j]);
-                     newSysArr.push(foundSystem[j]);
-                  }
-                  // console.log(newArr[i] + 'this');
+      Game.findById(req.params.id, function(err, foundGame){
+         console.log(foundGame.systems);
+         if (foundGame.systems.length < 2) {
+            for (var j = 0; j < foundSystem.length; j++) {
+               // console.log('%%%%%%%%%%%%%%%%%%%%');
+               // console.log(newArr[i]);
+               // console.log(foundSystem[j]._id);
+               // console.log('does they equal');
+               if (foundSystem[j]._id == foundGame.systems[0]){
+                  // console.log('yes');
+                  // console.log(foundSystem[j]);
+                  console.log('only one system');
+                  newSysArr = [foundSystem[j]];
+                  console.log(newSysArr);
                }
-            } else {
-               newSysArr.push(foundSystem[0]);
+            }
+               // console.log(newArr[i] + 'this');
+            // }
+         } else {
+            for (var i = 0; i < foundGame.systems.length; i++) {
+               // console.log(foundGame._id);
+               if (System.findById(foundGame.systems[i])){
+                  newSysArr.push(foundSystem[i]);
+                  console.log(newSysArr._id);
+               }
             }
          }
          // console.log(newSysArr);
          res.render('games/games_show.ejs', {
-            gamesArr: foundGames,
+            gamesArr: foundGame,
             systemsArr: newSysArr
          });
       });
@@ -64,6 +67,7 @@ router.get('/:id', function(req, res){
 //----------------------------------------------------------------
                         //Post Routes
 //----------------------------------------------------------------
+
 router.post('/', function(req, res){
    System.find({}, function(err, foundSystems){
       console.log(typeof req.body.systems);
@@ -72,36 +76,26 @@ router.post('/', function(req, res){
       console.log(req.body.systems);
       console.log(">>>>>>>>>>>>>>>>>>>>>>");
       var myArr = [];
-      // if (typeof req.body.systems == 'array') {
-      //    for (var i = 0; i < req.body.systems.length; i++) {
-      //       myArr.push(req.body.systems[i]);
-      //       console.log('Array');
-      //    }
-      // } else if(typeof req.body.systems == 'string'){
-      //    myArr.push(req.body.systems);
-      //    console.log('String');
-      // } else {
-      //    console.log("NOPE");
-      // }
+
       console.log(myArr);
       Game.create(req.body, function(err, createdGame){
-         console.log('++++++++++++++++++++++++++++');
-         console.log('req.body:');
-         console.log(req.body);
-         console.log('req.body.systems:');
-         console.log(req.body.systems);
-         console.log('req.body.systems.length');
-         console.log(req.body.systems.split(" "));
-         console.log('++++++++++++++++++++++++++++++');
+         // console.log('++++++++++++++++++++++++++++');
+         // console.log('req.body:');
+         // console.log(req.body);
+         // console.log('req.body.systems:');
+         // console.log(req.body.systems);
+         // console.log('req.body.systems.length');
+         // console.log(req.body.systems.length);
+         // console.log('++++++++++++++++++++++++++++++');
          for (var j = 0; j < foundSystems.length; j++) {
             console.log(typeof req.body.systems, req.body.systems, typeof req.body.systems === 'object');
             if (typeof req.body.systems === 'object') {
                console.log('object');
                for (var i = 0; i < req.body.systems.length; i++) {
-                  console.log('*********************');
-                  console.log(foundSystems[j]._id);
-                  console.log(req.body.systems[i]);
-                  console.log("*********************");
+                  // console.log('*********************');
+                  // console.log(foundSystems[j]._id);
+                  // console.log(req.body.systems[i]);
+                  // console.log("*********************");
                   if (foundSystems[j]._id == req.body.systems[i]) {
                      console.log('it matched');
                      foundSystems[j].games.push(req.body);
@@ -113,11 +107,14 @@ router.post('/', function(req, res){
             } else {
                console.log('not array');
                var elseArr = [];
-               elseArr.push(req.body.systems[0]);
-               if (foundSystems[j]._id == elseArr) {
+               console.log(elseArr, 'elseArr before push');
+               elseArr.push(req.body.systems);
+               console.log(elseArr, 'elseArr after push');
+               console.log(foundSystems[j]._id, elseArr[0] ,foundSystems[j]._id == elseArr[0]);
+               if (foundSystems[j]._id == elseArr[0]) {
                   console.log('it matched');
                   foundSystems[j].games.push(req.body);
-                  // console.log(foundSystem[j].games);
+                  console.log(foundSystems[j].games, 'after log');
                   foundSystems[j].save(function(err, savedSystem){
                   });
                }
@@ -134,8 +131,8 @@ router.delete('/:id', function(req, res){
    System.find({}, function(err, foundSystem){
       Game.findByIdAndRemove(req.params.id, function(err, foundGame){
          for (var i = 0; i < foundSystem.length; i++) {
-         foundSystem[i].games.id(req.params.id).remove();
-      }
+            foundSystem[i].games.id(req.params.id).remove();
+         }
       });
       foundSystem.save(function(err, savedSystem){
          res.redirect('/games');
